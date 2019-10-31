5f2b65c0fdfd6eaa722ec20206dd27f90b64c665
hizqeelrz <hizqeel1214@gmail.com>
2019-11-01 01:11:10 +0500

    add jwt/auth lib

--- mix.exs
+++ mix.exs
@@ -40,6 +40,9 @@ defmodule CashHold.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:comeonin, "~> 5.1"},
+      {:argon2_elixir, "~> 2.1"},
+      {:joken, "~> 2.1"},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
